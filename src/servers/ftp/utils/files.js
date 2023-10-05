d5cd73b30dd72cd086186da265264d7ad218557e
chbellec <cbellec@actioncontrelafaim.org>
2023-10-05 17:23:31 +0200

    fix linux compatiblity

--- src/servers/ftp/utils/files.js
+++ src/servers/ftp/utils/files.js
@@ -6,7 +6,8 @@ const today = new Date().toISOString().split("T")[0];
 const todayFilePath = path.join(outDir, today);
 
 function moveToPandoraBox(file, ip) {
-  const splitFilePath = file.split("\\");
+  const splitFilePath =
+    process.platform === "win32" ? file.split("\\") : file.split("/");
   const fileName = splitFilePath[splitFilePath.length - 1];
   const uploadFilePath = path.join(todayFilePath, ip);
   if (!fs.existsSync(todayFilePath)) {
